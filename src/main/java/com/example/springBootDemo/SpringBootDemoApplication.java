5d4e7f3f07f624c6078689e97ca5947417957ed9
Aishwarya Salunke <aishwaryasantoshsalunke@gmail.com>
2023-05-29 16:16:24 +0530

    Add. Get method

--- src/main/java/com/example/springBootDemo/SpringBootDemoApplication.java
+++ src/main/java/com/example/springBootDemo/SpringBootDemoApplication.java
@@ -2,12 +2,19 @@ package com.example.springBootDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemoApplication.class, args);
 	}
 
+	@GetMapping("/1")
+	public String hello() {
+		return "Hello World !";
+	}
 }
